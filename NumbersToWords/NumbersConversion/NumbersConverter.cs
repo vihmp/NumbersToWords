197520fece5d82fd0956aa16dd9697f87ff2ba45
vihmp <vihmp174@gmail.com>
2022-09-11 20:35:53 +0500

    implement SumProp

--- NumbersToWords/NumbersConversion/NumbersConverter.cs
+++ NumbersToWords/NumbersConversion/NumbersConverter.cs
@@ -30,9 +30,159 @@
                 return ConversionTables.NonGenderedNumbers[0][@case];
             }
 
+            ulong ones = nSum % 1000;
+            nSum /= 1000;
+            ulong thousands = nSum % 1000;
+            nSum /= 1000;
+            ulong millions = nSum % 1000;
+            nSum /= 1000;
+            ulong billions = nSum;
+
             var result = new StringBuilder();
 
-            result.Append(ConvertNumber(nSum, gender, @case));
+            if (ones > 0)
+            {
+                result.Append(ConvertOnes(ones, gender, @case));
+            }
+
+            if (thousands > 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Insert(0, " ");
+                }
+
+                result.Insert(0, ConvertThousands(thousands, @case));
+            }
+
+            if (millions > 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Insert(0, " ");
+                }
+
+                result.Insert(0, ConvertMillions(millions, @case));
+            }
+
+            if (billions > 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Insert(0, " ");
+                }
+
+                result.Insert(0, ConvertBillions(billions, @case));
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Converts numbers from 1 to 999 to words using specified gender and case
+        /// </summary>
+        /// <param name="number">Number</param>
+        /// <param name="gender">Gender</param>
+        /// <param name="case">Case</param>
+        /// <returns>Converted number</returns>
+        private string ConvertOnes(ulong number, Genders gender, Cases @case)
+        {
+            var result = new StringBuilder();
+
+            var (hundreds, tens, ones) = SplitNumber(number);
+
+            if (ones > 0)
+            {
+                result.Append(IsGenderedNumber(ones) ?
+                    ConversionTables.GenderedNumbers[ones][gender][@case] :
+                    ConversionTables.NonGenderedNumbers[ones][@case]);
+            }
+
+            if (tens > 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Insert(0, " ");
+                }
+
+                result.Insert(0, ConversionTables.NonGenderedNumbers[tens][@case]);
+            }
+
+            if (hundreds > 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Insert(0, " ");
+                }
+
+                result.Insert(0, ConversionTables.NonGenderedNumbers[hundreds][@case]);
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Converts specified number of thousands to words using specified case
+        /// </summary>
+        /// <param name="number">Number of thousands</param>
+        /// <param name="case">Case</param>
+        /// <returns>Converted number</returns>
+        private string ConvertThousands(ulong number, Cases @case)
+        {
+            var thousandGrammaticalNumber = 
+                GetGrammaticalNumber(number, @case, Genders.Feminine);
+            var thousandCase = 
+                GetCase(number, @case, Genders.Feminine);
+
+            var result = new StringBuilder();
+
+            result.Append(ConvertOnes(number, Genders.Feminine, @case));
+            result.Append(" ");
+            result.Append(ConversionTables.Thousand[thousandGrammaticalNumber][thousandCase]);
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Converts specified number of millions to words using specified case
+        /// </summary>
+        /// <param name="number">Number of millions</param>
+        /// <param name="case">Case</param>
+        /// <returns>Converted number</returns>
+        private string ConvertMillions(ulong number, Cases @case)
+        {
+            var millionGrammaticalNumber = 
+                GetGrammaticalNumber(number, @case, Genders.Masculine);
+            var millionCase = 
+                GetCase(number, @case, Genders.Masculine);
+
+            var result = new StringBuilder();
+
+            result.Append(ConvertOnes(number, Genders.Masculine, @case));
+            result.Append(" ");
+            result.Append(ConversionTables.Million[millionGrammaticalNumber][millionCase]);
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Converts specified number of billions to words using specified case
+        /// </summary>
+        /// <param name="number">Number of billions</param>
+        /// <param name="case">Case</param>
+        /// <returns>Converted number</returns>
+        private string ConvertBillions(ulong number, Cases @case)
+        {
+            var billionGrammaticalNumber = 
+                GetGrammaticalNumber(number, @case, Genders.Masculine);
+            var billionCase = 
+                GetCase(number, @case, Genders.Masculine);
+
+            var result = new StringBuilder();
+
+            result.Append(ConvertOnes(number, Genders.Masculine, @case));
+            result.Append(" ");
+            result.Append(ConversionTables.Billion[billionGrammaticalNumber][billionCase]);
 
             return result.ToString();
         }
@@ -84,49 +234,6 @@
         }
 
         /// <summary>
-        /// Converts numbers from 1 to 999 to words using specified gender and case
-        /// </summary>
-        /// <param name="number">Number</param>
-        /// <param name="gender">Gender</param>
-        /// <param name="case">Case</param>
-        /// <returns>Converted number</returns>
-        private string ConvertNumber(ulong number, Genders gender, Cases @case)
-        {
-            var result = new StringBuilder();
-
-            var (hundreds, tens, ones) = SplitNumber(number);
-
-            if (ones > 0)
-            {
-                result.Append(IsGenderedNumber(ones) ?
-                    ConversionTables.GenderedNumbers[ones][gender][@case] :
-                    ConversionTables.NonGenderedNumbers[ones][@case]);
-            }
-
-            if (tens > 0)
-            {
-                if (result.Length > 0)
-                {
-                    result.Insert(0, " ");
-                }
-
-                result.Insert(0, ConversionTables.NonGenderedNumbers[tens][@case]);
-            }
-
-            if (hundreds > 0)
-            {
-                if (result.Length > 0)
-                {
-                    result.Insert(0, " ");
-                }
-
-                result.Insert(0, ConversionTables.NonGenderedNumbers[hundreds][@case]);
-            }
-
-            return result.ToString();
-        }
-
-        /// <summary>
         /// Splits number from 1 to 999 into hundreds, tens and ones
         /// </summary>
         /// <param name="number"></param>
@@ -160,6 +267,106 @@
         private bool IsGenderedNumber(ulong number)
         {
             return (number == 1) || (number == 2);
+        }
+
+        /// <summary>
+        /// Returns grammatical number of 'thousand'/'million'/'billion' word
+        /// following specified number (that is written in specified case)
+        /// </summary>
+        /// <param name="number">Number</param>
+        /// <param name="case">Case of converted number</param>
+        /// <param name="gender">Gender of 'thousand'/'million'/'billion' word</param>
+        /// <returns>Grammatical number</returns>
+        private GrammaticalNumbers GetGrammaticalNumber(ulong number, Cases @case, Genders gender)
+        {
+            // We suppose that 'number' is greater than zero, so 'hundreds' or 'tens' 
+            // should be greater than zero if 'ones' = 0
+            var (_, _, ones) = SplitNumber(number);
+
+            if (gender == Genders.Feminine)
+            {
+                return (ones == 1) ?
+                    GrammaticalNumbers.Singular :
+                    GrammaticalNumbers.Plural;
+            }
+            else
+            {
+                switch (ones)
+                {
+                    case 1:
+                        return GrammaticalNumbers.Singular;
+                    case 2:
+                    case 3:
+                    case 4:
+                        if ((@case == Cases.Nominative) ||
+                            (@case == Cases.Accusative))
+                        {
+                            return GrammaticalNumbers.Singular;
+                        }
+                        else
+                        {
+                            return GrammaticalNumbers.Plural;
+                        }
+                    default:
+                        return GrammaticalNumbers.Plural;
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Returns case of 'thousand'/'million'/'billion' word following specified 
+        /// number (that is written in specified case)
+        /// </summary>
+        /// <param name="number">Number</param>
+        /// <param name="case">Case of converted number</param>
+        /// <returns>Case</returns>
+        private Cases GetCase(ulong number, Cases @case, Genders gender)
+        {
+            // We suppose that 'number' is greater than zero, so 'hundreds' or 'tens' 
+            // should be greater than zero if 'ones' = 0
+            var (_, _, ones) = SplitNumber(number);
+
+            if (gender == Genders.Feminine)
+            {
+                switch (ones)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        return @case;
+                    default:
+                        if ((@case == Cases.Nominative) ||
+                            (@case == Cases.Accusative))
+                        {
+                            return Cases.Genitive;
+                        }
+                        else
+                        {
+                            return @case;
+                        }
+                }
+            }
+            else
+            {
+                if (ones == 1)
+                {
+                    return @case;
+                }
+                else
+                {
+                    if ((@case == Cases.Nominative) ||
+                        (@case == Cases.Accusative))
+                    {
+                        return Cases.Genitive;
+                    }
+                    else
+                    {
+                        return @case;
+                    }
+                }
+            }
         }
     }
 }
--- NumbersToWords/Program.cs
+++ NumbersToWords/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NumbersToWords
+﻿namespace NumbersToWords
 {
+    using NumbersToWords.NumbersConversion;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var converter = new NumbersConverter();
+
+            Console.WriteLine(converter.SumProp(31, "М", "Р"));
+            Console.WriteLine(converter.SumProp(22, "С", "Т"));
+            Console.WriteLine(converter.SumProp(154323, "М", "И"));
+            Console.WriteLine(converter.SumProp(154323, "М", "Т"));
         }
     }
 }
